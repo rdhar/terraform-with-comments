2f5eb6fafdeb0ffa72f94cffd598c8f5998febd8
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-05-21 18:56:03 +0100

    build: use s3 bucket module

--- stacks/sample_bucket/main.tf
+++ stacks/sample_bucket/main.tf
@@ -1,23 +1,13 @@
-# Create a sample S3 bucket.
-resource "aws_s3_bucket" "sample" {
-  bucket_prefix = var.PREFIX
+# Create a sample S3 bucket with versioning.
+module "sample_bucket" {
+  source = "../modules/s3_bucket"
 
-  tags = {
-    Name = join("-", [var.PREFIX, "bucket"])
-  }
-}
-
-# Set versioning on the sample S3 bucket.
-resource "aws_s3_bucket_versioning" "sample" {
-  bucket = aws_s3_bucket.sample.id
-
-  versioning_configuration {
-    status = var.bucket_versioning
-  }
+  PREFIX            = var.PREFIX
+  bucket_versioning = var.bucket_versioning
 }
 
 # Output the ID of the sample S3 bucket.
 output "sample_bucket_id" {
   description = "ID of the sample S3 bucket."
-  value       = aws_s3_bucket.sample.id
+  value       = module.sample_bucket.id
 }
